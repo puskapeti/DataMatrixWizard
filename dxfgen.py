3fe284489360bb7ae00ea9f6ea5374e17861acff
puskapeti <puskapeti12@gmail.com>
2021-11-10 14:33:49 +0100

    Implemented multiple layer generation

--- dxfgen.py
+++ dxfgen.py
@@ -6,6 +6,8 @@ from Matrix import *
 
 class Generator:
     SECTOR_NAME = "SECTOR"
+    HATCH_NAME = "HATCH"
+    OUTLINE_NAME = "OUTLINE"
     SECTOR_WIDTH = 2
 
     def __init__(self, matrix: Matrix, hatch_angle=0, hatch_density=10):
@@ -13,28 +15,42 @@ class Generator:
         self.__hatch_angle = hatch_angle
         self.__hatch_density = hatch_density
 
-        self.__doc = ezdxf.new("R2010")  # creates a new DXF 2010 drawing
+        self.__doc = ezdxf.new("R2010", setup=True)  # creates a new DXF 2010 drawing
 
         self.__hatch = None
 
     def generate(self):
         msp = self.__doc.modelspace()  # adds modelspace
 
-        self.__generate_block()  # generate block references
+        # generate layers
+        self.__doc.layers.add(name=Generator.HATCH_NAME)
+        self.__doc.layers.add(name=Generator.OUTLINE_NAME)
+
+        self.__generate_blocks()  # generate block references
 
         points = self.__matrix.generate_points()  # generate point coordinates from the matrix
 
         sw = Generator.SECTOR_WIDTH
 
         for point in points:
-            msp.add_blockref("SECTOR", (point.x * sw, point.y * sw),
+            # add the hatch to the hatch layer
+            msp.add_blockref(Generator.HATCH_NAME, (point.x * sw, point.y * sw),
                              dxfattribs={
                                  'xscale': 1,
                                  'yscale': 1,
-                                 'rotation': 0
+                                 'rotation': 0,
+                                 'layer': Generator.HATCH_NAME
+                             })
+            # add the outline to the outline layer
+            msp.add_blockref(Generator.OUTLINE_NAME, (point.x * sw, point.y * sw),
+                             dxfattribs={
+                                 'xscale': 1,
+                                 'yscale': 1,
+                                 'rotation': 0,
+                                 'layer': Generator.OUTLINE_NAME
                              })
 
-        # generate frame
+        # add the frame to the outline layer
         msp.add_lwpolyline(
             [
                 (-sw/2, -sw/2),
@@ -42,16 +58,20 @@ class Generator:
                 (self.__matrix.width * sw - sw/2, self.__matrix.height * sw - sw/2),
                 (-sw/2, self.__matrix.height * sw - sw/2),
                 (-sw / 2, -sw / 2)
-            ]
+            ],
+            dxfattribs={
+                'layer': Generator.OUTLINE_NAME
+            }
         )
 
         self.__doc.saveas('generated.dxf')
 
-    def __generate_block(self):
-        sector = self.__doc.blocks.new(name=Generator.SECTOR_NAME)  # generate block named SECTOR
+    def __generate_blocks(self):
+        hatch_block = self.__doc.blocks.new(name=Generator.HATCH_NAME)
+        outline_block = self.__doc.blocks.new(name=Generator.OUTLINE_NAME)
 
-        hatch = sector.add_hatch()
-        hatch.set_pattern_fill('ANSI31', scale=1/self.__hatch_density)
+        hatch = hatch_block.add_hatch()
+        hatch.set_pattern_fill('ANSI31', scale=1 / self.__hatch_density)
         hatch.set_pattern_angle(self.__hatch_angle)
         dist = Generator.SECTOR_WIDTH / 2
         hatch.paths.add_polyline_path(
@@ -63,7 +83,8 @@ class Generator:
             ],
             is_closed=True
         )
-        sector.add_lwpolyline(
+
+        outline_block.add_lwpolyline(
             [
                 (dist, dist),
                 (dist, -dist),
